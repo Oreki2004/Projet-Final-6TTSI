89814d874ea9c59a02db9650504b20cc27863b27
Oreki2004 <97853712+Oreki2004@users.noreply.github.com>
2024-03-28 23:06:13 +0100

    Add files via upload

---
+++ map.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URD - MAP</title>
+</head>
+<body>
+<!-- Image Map généré par http://www.image-map.net/ -->
+<img src="IMG/destiny2_maps_director_s4.jpg" usemap="#image-map">
+
+<map name="image-map">
+    <area target="_blank" alt="" title="" href="io" coords="239,200,40" shape="circle">
+    <area target="_blank" alt="" title="" href="terre" coords="613,348,74" shape="circle">
+    <area target="_blank" alt="" title="" href="nessos" coords="815,142,34" shape="circle">
+    <area target="_blank" alt="" title="" href="titan" coords="335,450,37" shape="circle">
+</map>
+</body>
+</html>
