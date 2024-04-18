004a448f4bc61a5ed77c9bf4682a256b5f0c00b7
Oreki2004 <97853712+Oreki2004@users.noreply.github.com>
2024-04-18 16:12:18 +0200

    Add files via upload

--- valider_quiz.php
+++ valider_quiz.php
@@ -116,32 +116,28 @@
                               </li>';
                     } else {
                         echo '<li><a href="connexion.php">CONNEXION</a></li>';
-                    }
+                    } 
                 ?>
             </ul>
         </nav>
     </header>
     <?php
-// Connexion à la base de données
+
 $mysqli = new mysqli("localhost", "root", "", "urd");
 
-// Vérification de la connexion
 if ($mysqli->connect_error) {
     die("La connexion à la base de données a échoué : " . $mysqli->connect_error);
 }
 
-// Récupération du score actuel du joueur
-$joueur = $_SESSION['pseudo']; // Remplacez "Nom du joueur" par le nom du joueur
+$joueur = $_SESSION['pseudo'];
 $query_select_score = "SELECT score FROM score_quiz WHERE joueur = '$joueur'";
 $result_select_score = $mysqli->query($query_select_score);
 
 if ($result_select_score) {
-    // Si le joueur existe dans la base de données
     if ($result_select_score->num_rows > 0) {
         $row = $result_select_score->fetch_assoc();
         $score_joueur = $row['score'];
 
-        // Calcul du score actuel
         $score_actuel = 0;
         $reponses_utilisateur = isset($_POST['reponses']) ? $_POST['reponses'] : array();
         foreach ($reponses_utilisateur as $id_question => $reponse_utilisateur) {
@@ -155,7 +151,7 @@ if ($result_select_score) {
             }
         }
 
-        // Mise à jour du score si nécessaire
+
         if ($score_actuel > $score_joueur) {
             $query_update_score = "UPDATE score_quiz SET score = $score_actuel WHERE joueur = '$joueur'";
             $result_update_score = $mysqli->query($query_update_score);
@@ -168,8 +164,7 @@ if ($result_select_score) {
             echo "Ton score actuel est de ".$score_actuel.", tu peux faire mieux.";
         }
     } else {
-        // Si le joueur n'existe pas dans la base de données, on insère son score
-        // Calcul du score actuel
+
         $score_actuel = 0;
         $reponses_utilisateur = isset($_POST['reponses']) ? $_POST['reponses'] : array();
         foreach ($reponses_utilisateur as $id_question => $reponse_utilisateur) {
@@ -182,13 +177,25 @@ if ($result_select_score) {
                 }
             }
         }
-        // Insertion du score
+        if (isset($_POST['reponses'])) {
+            
+            $query_niv = "UPDATE niveau SET niveau ='2' WHERE joueur = '$joueur'";
+            $resultat_niv = $mysqli->query($query_niv);
+            if ($resultat_niv) {
+                echo "Vous avez maintenant accés au Niveau 2.";
+                
+                
+            } else {
+                echo "Erreur lors de la mise à jour du niveau : " . $mysqli->error;
+            }
+        }
+
+
         $query_score = "INSERT INTO score_quiz (joueur, score) VALUES ('$joueur', $score_actuel)";
         $resultat_score = $mysqli->query($query_score);
         if ($resultat_score) {
             echo "Score inséré avec succès.";
             
-
         } else {
             echo "Erreur lors de l'insertion du score : " . $mysqli->error;
         }
@@ -197,7 +204,6 @@ if ($result_select_score) {
     echo "Erreur lors de la récupération du score : " . $mysqli->error;
 }
 
-// Fermeture de la connexion à la base de données
 $mysqli->close();
 ?>
 
